5c30fd94409dad2126e46d6bad8881d3434fd3dd
Cristofer Holobetz <cristofer.holobetz@berkeley.edu>
2024-05-23 10:33:09 +0000

    Generate update rule comparison figure with fixed initial conditions

--- scripts/import_jax.py
+++ scripts/import_jax.py
@@ -16,16 +16,21 @@ def two_layer_network_loss(W, x, target_multiple):
 rng = np.random.default_rng()
 
 init_weights_list = []
-final_weights_list = []
+grad_final_weights_list = []
+newton_final_weights_list = []
 
-for i in np.arange(100):
+for initial_condition in np.arange(1, 101):
 
   TARGET_MULTIPLE = 2
   W0 = rng.uniform(-4, 4, size=2) + 1e-3
-  W = W0
+  W_grad = W0
+  W_newton = W0
 
-  for epoch in np.arange(1, 10):
-    
+  for epoch in np.arange(1, 100):
+
+    print(f'Initial condition {initial_condition}')
+    print('Epoch {0}'.format(epoch))
+
     input = jnp.array(rng.poisson(4, size=1) + 1e-3)
 
     def two_layer_network_loss(W, x, target_multiple):
@@ -33,38 +38,42 @@ for i in np.arange(100):
       return jnp.mean((W[0] * W[1] * x - target_multiple * x)**2)
 
     grad_network = grad(two_layer_network_loss, argnums=(0))
-    gradient = grad_network(W, input, TARGET_MULTIPLE)
+    gradient1 = grad_network(W_grad, input, TARGET_MULTIPLE)
 
     def hessian(f):
         return jacfwd(jacrev(f))
     
-    loss = two_layer_network_loss(W, input, TARGET_MULTIPLE)
+    loss = two_layer_network_loss(W_newton, input, TARGET_MULTIPLE)
 
-    H = hessian(two_layer_network_loss)(W, input, TARGET_MULTIPLE)
-    H_inv = jnp.linalg.inv(H)
-    print(f'H: {H}')
-    print(f'H_inv: ')
-    print(f'loss: {loss}\n \n')
-    print('Epoch {0}'.format(epoch))
-
-    update_vector = jnp.linalg.matmul(H_inv, gradient)
-    
     if loss > 0:
 
-      W = W - jnp.linalg.matmul(H_inv, gradient)
+      gradient2 = grad_network(W_newton, input, TARGET_MULTIPLE)
 
-    #W = W - 1e-3 * gradient
+      H = hessian(two_layer_network_loss)(W_newton, input, TARGET_MULTIPLE)
+      H_inv = jnp.linalg.inv(H)
+      print(f'H: {H}')
+      print(f'H_inv: ')
+      print(f'loss: {loss}\n \n')
+
+      newton_update_vector = jnp.linalg.matmul(H_inv, gradient2)
+
+      W_newton = W_newton - newton_update_vector
+
+    W_grad = W_grad - 1e-3 * gradient1
 
   init_weights_list.append(W0)
-  final_weights_list.append(W)
+  grad_final_weights_list.append(W_grad)
+  newton_final_weights_list.append(W_newton)
 
 init_weights_arr = np.array(init_weights_list)
-final_weights_arr = np.array(final_weights_list)
+grad_final_weights_arr = np.array(grad_final_weights_list)
+newton_final_weights_arr = np.array(newton_final_weights_list)
 
 fig, ax = plt.subplots()
 
 ax.scatter(init_weights_arr[:, 0], init_weights_arr[:, 1], color='tab:blue', label='initial weights')
-ax.scatter(final_weights_arr[:, 0], final_weights_arr[:, 1], color='tab:orange', label='final weights')
+ax.scatter(grad_final_weights_arr[:, 0], grad_final_weights_arr[:, 1], color='tab:orange', label='final weights under GD')
+ax.scatter(newton_final_weights_arr[:, 0], newton_final_weights_arr[:, 1], color='tab:purple', label='final weights under NM')
 
 target_curve_x_left = np.linspace(-4, 0, 500)
 target_curve_x_right = np.linspace(0, 4, 500)
@@ -77,12 +86,14 @@ ax.plot(target_curve_x_right, target_curve_y_right, color='tab:green')
 
 for i in np.arange(init_weights_arr.shape[0]):
 
-  ax.plot([init_weights_arr[i, 0], final_weights_arr[i, 0]], [init_weights_arr[i, 1], final_weights_arr[i, 1]])
+  ax.plot([init_weights_arr[i, 0], grad_final_weights_arr[i, 0]], [init_weights_arr[i, 1], grad_final_weights_arr[i, 1]], color='tab:orange')
+  ax.plot([init_weights_arr[i, 0], newton_final_weights_arr[i, 0]], [init_weights_arr[i, 1], newton_final_weights_arr[i, 1]], color='tab:purple')
+
 
 ax.set_xlim(-4, 4)
 ax.set_ylim(-4, 4)
 
-ax.set_title('Learned weights under Newton\'s Method')
+ax.set_title('Learned weights under different update rules')
 ax.set_xlabel('weight 1 value')
 ax.set_ylabel('weight 2 value')
 
